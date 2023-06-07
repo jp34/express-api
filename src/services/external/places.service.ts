3f9508093fa6a5b682dbffc9c4a776379cb622d7
jp34 <joekadlic@outlook.com>
2023-06-07 13:07:42 -0400

    Added places.service

---
+++ src/services/external/places.service.ts
@@ -0,0 +1,28 @@
+import axios from "axios";
+import { ConfigurationError, ServerError } from "../../models/error";
+
+const API_FOURSQUARE_KEY = process.env.API_FOURSQUARE_KEY ?? undefined;
+
+export type PlacesSearchOptions = {
+    query: string | undefined,
+    categories: string | undefined,
+    geo: {
+        latitude: string,
+        longitude: string
+    },
+    radius: number,
+};
+
+export const placesSearch = async (options: PlacesSearchOptions) => {
+    if (!API_FOURSQUARE_KEY) throw new ConfigurationError('Environment variable API_FOURSQUARE_KEY is missing or invalid');
+    axios.get(`https://api.foursquare.com/v3/places/search?${options.toString()}`, {
+        headers: {
+            Authorization: API_FOURSQUARE_KEY,
+            Accept: 'application/json'
+        }}
+    ).then((response) => {
+        return response.data.results;
+    }).catch((err: any) => {
+        throw new ServerError(err);
+    });
+}
